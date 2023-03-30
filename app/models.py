59a68c65089a77ec25b32da173d4b8aa3eec386a
timgio26 <timgio@outlook.com>
2023-03-30 15:03:24 +0700

    first commit

---
+++ app/models.py
@@ -0,0 +1,32 @@
+from app import db
+
+class Warehouse(db.Model):
+    __tablename__ = 'warehouse'
+    id = db.Column(db.Integer, primary_key=True)
+    warehouse_id = db.Column(db.String(10))
+    warehouse_name = db.Column(db.String(120))
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.String(25),index=True, unique=True)
+    product_name = db.Column(db.String(150))
+    product_sku = db.Column(db.String(25))
+    product_status = db.Column(db.String(10))
+    # stok_hist = db.relationship('Stok', backref='itemdetail', lazy='dynamic')
+
+class Credential(db.Model):
+    __tablename__ = 'credential'
+    id = db.Column(db.Integer, primary_key=True)
+    appId = db.Column(db.String(50))
+    clientId = db.Column(db.String(50))
+    clientSecret= db.Column(db.String(50))
+    clientBearer = db.Column(db.String(50))
+
+class Stok(db.Model):
+    __tablename__ = 'stok'
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.String(25))
+    item_qty = db.Column(db.Integer)
+    stok_date = db.Column(db.Date)
+    wh_id = db.Column(db.String(25))
---
+++ app/routes.py
@@ -0,0 +1,110 @@
+from flask import render_template,redirect, url_for,request
+from app import app,db,api
+from app.models import Warehouse,Product,Credential,Stok
+from datetime import date
+from flask_restful import Resource
+from sqlalchemy import create_engine
+import pandas as pd
+import json
+
+class apiv1(Resource):
+    def post(self):
+        id=request.args.get('id')
+        qty=request.args.get('qty')
+        wh=request.args.get('wh')
+        print(id)
+        print(qty)
+        newdata=Stok(item_id=id,item_qty=qty,stok_date=date.today(),wh_id=wh)
+        db.session.add(newdata)
+        db.session.commit()
+        return {'status':'added stok'}
+    def get(self):
+        if request.args.get('data')=="product":
+            engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+            df = pd.read_sql_query("SELECT * FROM Product", con=engine)
+            engine.dispose()
+            df=df.to_json(orient='records')
+            df=json.loads(df)
+            return df
+        elif (request.args.get('data')=="credentials"):
+            if request.args.get('secretkey')==app.config['SECRET_KEY']:
+                engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+                df = pd.read_sql_query("SELECT * FROM Credential", con=engine)
+                engine.dispose()
+                df=df.to_json(orient='records')
+                df=json.loads(df)
+                return df
+            else:
+                return {'status':'wrong secret key'}
+        else:
+            engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+            df = pd.read_sql_query("SELECT * FROM Warehouse", con=engine)
+            engine.dispose()
+            df=df.to_json(orient='records')
+            df=json.loads(df)
+            return df
+        
+api.add_resource(apiv1,'/apiv1')
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/stokhist')
+def stokhist():
+    df=Stok.query.all()
+    return render_template('stokhist.html',df=df)
+
+@app.route('/delstokhist/<id>')
+def delstokhist(id):
+    itemdel=Stok.query.get(id)
+    db.session.delete(itemdel)
+    db.session.commit()
+    return redirect(url_for('stokhist'))
+
+@app.route('/wh', methods=['GET', 'POST'])
+def wh():
+    df=Warehouse.query.all()
+    if request.method=='POST':
+        NewWh=Warehouse(warehouse_id=request.form['InpWhId'],warehouse_name=request.form['InpWhName'])
+        db.session.add(NewWh)
+        db.session.commit()
+        return redirect(url_for('wh'))
+    else:        
+        return render_template('wh.html',df=df)
+    
+@app.route('/product', methods=['GET', 'POST'])
+def prod():
+    df=Product.query.all()
+    if request.method=='POST':
+        NewProd=Product(product_id=request.form['InpProdId'],product_status="Active")
+        db.session.add(NewProd)
+        db.session.commit()
+        return redirect(url_for('prod'))
+    else:        
+        return render_template('product.html',df=df)
+    
+@app.route('/cred', methods=['GET', 'POST'])
+def cred():
+    df=Credential.query.get(1)
+    if request.method=='POST':
+        if request.form['action']=="Add Credential":
+            NewCred=Credential(appId=request.form['InpAppId'],
+                               clientId=request.form['InpClientId'],
+                               clientSecret=request.form['InpClientSecret'],
+                               clientBearer=request.form['InpClientBearer'])
+            db.session.add(NewCred)
+            db.session.commit()
+            return redirect(url_for('cred'))
+        else:
+            df.appId=request.form['InpAppId']
+            df.clientId=request.form['InpClientId']
+            df.clientSecret=request.form['InpClientSecret']
+            df.clientBearer=request.form['InpClientBearer']
+            db.session.add(df)
+            db.session.commit()
+            # credential.query.all()
+            return redirect(url_for('cred'))
+    else:        
+        return render_template('credential.html',df=df)
