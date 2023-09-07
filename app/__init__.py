fcf1839459672e8899357d37cd53c726843c2f52
timgio26 <timgio@outlook.com>
2023-09-07 22:46:20 +0700

    add cors 2

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource,Api
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app,origins=["https://www.nutrimart.co.id"])
 app.config.from_pyfile('config.py')
 api=Api(app)
 db = SQLAlchemy(app)
